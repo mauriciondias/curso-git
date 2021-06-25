a8cb475af518e89a678f2b28f5dec670b5760812
mauriciondias <mauricionpdias@hotmail.com>
2021-06-25 21:00:05 +0100

    Acrescentada a mensagem tenha um bom dia

--- git/Program.cs
+++ git/Program.cs
@@ -5,6 +5,7 @@ namespace git {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Tenha um bom dia");
             Console.ReadLine();
         }
     }
