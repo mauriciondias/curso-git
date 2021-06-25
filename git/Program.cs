bf937d53ef4b97370612da169d30ef2f7b983573
mauriciondias <mauricionpdias@hotmail.com>
2021-06-25 20:02:52 +0100

    Projeto acrescentado com a expressao Bom Dia

--- git/Program.cs
+++ git/Program.cs
@@ -4,6 +4,8 @@ namespace git {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
+            Console.ReadLine();
         }
     }
 }
